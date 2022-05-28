3f99059636fa366512b71f71104624ce93ad984f
vika <vikus200@rambler.ru>
2022-05-28 11:54:14 +0300

    new class

---
+++ External.java
@@ -0,0 +1,9 @@
+public class External {
+private int code;
+private String name;
+
+    public External(int code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+}
--- L1.java
+++ L1.java
@@ -1,5 +1,8 @@
 public class L1 {
     public static void main(String[] args) {
         System.out.println("Hello");
+
+        External cl = new External(1, "soul");
     }
 }
+
